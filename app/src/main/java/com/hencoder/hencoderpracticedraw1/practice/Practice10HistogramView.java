69fee6343cc3743ddc75e549d365fa2c0ca0dd81
HuangJianHong <18030221649@163.com>
2017-12-07 15:37:58 +0800

    [ADD] 画直方图, 但是横坐标没有居中显示;

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
@@ -21,8 +21,6 @@ public class Practice10HistogramView extends View {
     public Practice10HistogramView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(Color.WHITE);
         path = new Path();
     }
 
@@ -36,20 +34,32 @@ public class Practice10HistogramView extends View {
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.WHITE);
         path.moveTo(100, 100);
         path.rLineTo(0, 500);
-        path.rLineTo(800, 0);
+        path.rLineTo(1200, 0);
+        canvas.drawPath(path, paint);    //画坐标轴
 
-        canvas.drawPath(path, paint);
-        paint.setTextSize(20);
-        canvas.drawText("froyo", 120, 630, paint);
-        canvas.drawText("GB", 150, 630, paint);
-        canvas.drawText("ICS", 180, 630, paint);
-        canvas.drawText("JB", 210, 630, paint);
-        canvas.drawText("KitKat", 240, 630, paint);
-        canvas.drawText("L", 270, 630, paint);
-        canvas.drawText("M", 280, 630, paint);
-//        canvas.drawText();
+        int lefMargin = 120;
+        int dx = 100;                       //直方图宽度
+        int divider = 30;                   //直方图的间隔
 
+        paint.setTextSize(40);
+        String title[] ={"froyo", "GB", "ICS", "JB", "KitKat" , "L", "M"};
+        for (int i = 0; i < title.length; i++) {
+            canvas.drawText(title[i], i*dx+ (i+1)*divider + lefMargin, 630, paint);
+        }
+        canvas.drawText( "直方图", 400,700, paint);                   //画横坐标
+
+
+        paint.setColor(Color.GREEN);             //画直方图
+        paint.setStyle(Paint.Style.FILL);
+        int height[] = {300,500,100,200,480,460,570};
+        for (int i = 0; i < height.length; i++) {
+            canvas.drawRect(i*dx+ (i+1)*divider + lefMargin, height[i], (i+1)*(dx+divider) + lefMargin, 600, paint);
+        }
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.WHITE);
     }
 }
